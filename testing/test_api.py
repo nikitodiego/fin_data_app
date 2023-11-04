a2bbea07ae7daab0827d290df294f06e0a3bd2c7
nikitodiego <nicojapaz@gmail.com>
2023-11-04 14:00:55 -0300

    second commit

---
+++ testing/test_api.py
@@ -0,0 +1,11 @@
+import requests
+import pytest
+from datetime import date
+
+#definiendo funcion
+def test_get_api():
+    response = requests.get(
+        f"https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/{date.today()}?adjusted=true&apiKey={secrets.API_KEY}"
+    )
+    assert response.status_code ==200
+    assert response.json()['resultsCount'] >=0
