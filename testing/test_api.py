e575789b9ec2d754de90a64650b159c510b02e08
nikitodiego <nicojapaz@gmail.com>
2023-11-04 15:01:10 -0300

    date

--- testing/test_api.py
+++ testing/test_api.py
@@ -1,12 +1,11 @@
 import requests
 import pytest
-from datetime import date
 import os
 
 #definiendo funcion
 def test_get_api():
     response = requests.get(
-        f"https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/{date.today()}?adjusted=true&apiKey={os.getenv('api_key')}"
+        f"https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/2023-07-26?adjusted=true&apiKey={os.getenv('api_key')}"
     )
     assert response.status_code ==200
     assert response.json()['resultsCount'] >=0
