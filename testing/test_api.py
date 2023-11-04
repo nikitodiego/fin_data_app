a42372fd188169ff7d8b74c43cc133225fff16c0
nikitodiego <nicojapaz@gmail.com>
2023-11-04 14:35:04 -0300

    dotenv

--- testing/test_api.py
+++ testing/test_api.py
@@ -2,12 +2,13 @@ import requests
 import pytest
 from datetime import date
 import os
+from dotenv import load_dotenv
 
-API_KEY=os.environ["API_KEY"]
+load_dotenv()
 #definiendo funcion
 def test_get_api():
     response = requests.get(
-        f"https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/{date.today()}?adjusted=true&apiKey={API_KEY}"
+        f"https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/{date.today()}?adjusted=true&apiKey={os.getenv('API_KEY')}"
     )
     assert response.status_code ==200
     assert response.json()['resultsCount'] >=0
