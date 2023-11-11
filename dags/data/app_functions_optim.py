2c6b207ff90c059ac6186f4e4db4276ca445a251
nikitodiego <nicojapaz@gmail.com>
2023-11-11 17:28:58 -0300

    mejoras

--- dags/data/app_functions_optim.py
+++ dags/data/app_functions_optim.py
@@ -1,61 +1,53 @@
-from datetime import datetime
+import datetime
 import pandas as pd
-from google.cloud import storage
 from google.cloud import bigquery
 import os
-import json
 import requests
-import gcsfs
 import time
 from dotenv import load_dotenv
+import logging
 
 load_dotenv()
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r'/opt/airflow/dags/data/credentials.json'
 
 def api_call(**context):
     time.sleep(30)
     date=context['ds']
-    try:
-        response = requests.get(f"https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/{date}?adjusted=true&apiKey={os.getenv('API_KEY')}")
-        data = response.json()
+    if (0<= datetime.datetime.strptime(date,'%Y-%m-%d').date().weekday()<5):
+        try:
+            response = requests.get(f"https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/{date}?adjusted=true&apiKey={os.getenv('API_KEY')}")
+            data = response.json()
+        except: 
+            logging.error(f'{response.status_code}')
         if (data["resultsCount"]>0): 
             df = pd.DataFrame(data["results"])
             df2=df[["T", "o","c","h","l"]]
-            with open('/opt/airflow/dags/data/credentials.json') as jsonFile:
-                jsonObject = json.load(jsonFile)
-                df2.to_parquet(f'gcs://bucket_dev01/data/{date}.parquet',storage_options={"token": jsonObject})
-                jsonFile.close()
+            df2.rename(columns = {'T':'Ticker','o':'open','c':'close','h':'high','l':'low'}, inplace = True)
+            df2.to_parquet(f'gcs://bucket_dev01/data/{date}.parquet')
             return {"msg":"Data loaded"}
-        else:
-            return {"msg":"No market ops day"}        
-    except:
-        print(f"Error code: {response.status_code}")
+        else: logging.info('No market ops day')
+    else: logging.info('Weekend day')        
 
 
 def bucket_to_bq(**context):
-    # Construct a BigQuery client object.
-    try:
+    date=context['ds']
+    xcom_object= context['task_instance'].xcom_pull(task_ids='llamada_a_api')
+    if xcom_object=={"msg":"Data loaded"}:
         client = bigquery.Client()
-        date=context['ds']
         df = pd.read_parquet(f'gcs://bucket_dev01/data/{date}.parquet')
         df["date"]=date
-        # TODO(developer): Set table_id to the ID of the table to create.
-        table_id = "analog-provider-400614.dataset_dev.tickers_optim"
+        table_id = "analog-provider-400614.dataset_dev.tickers_optim_named"
 
         job_config = bigquery.LoadJobConfig(
         schema=[
-            bigquery.SchemaField("T", "STRING"),
-            bigquery.SchemaField("o", "FLOAT"),
-            bigquery.SchemaField("c", "FLOAT"),
-            bigquery.SchemaField("h", "FLOAT"),
-            bigquery.SchemaField("l", "FLOAT"),
+            bigquery.SchemaField("Ticker", "STRING"),
+            bigquery.SchemaField("open", "FLOAT"),
+            bigquery.SchemaField("close", "FLOAT"),
+            bigquery.SchemaField("high", "FLOAT"),
+            bigquery.SchemaField("low", "FLOAT"),
             bigquery.SchemaField("date", "STRING")
-        ],
-        #skip_leading_rows=1,
-        )
-        load_job = client.load_table_from_dataframe(
-            df, table_id, job_config=job_config
-        )  # Make an API request.
-        load_job.result()  # Waits for the job to complete.
-    except: return "No file to load"
+        ],)
+        load_job = client.load_table_from_dataframe(df, table_id, job_config=job_config)  
+        load_job.result() 
+        return {"msg":"Data loaded in BigQuery"}
+    else: return {"msg":"No file found"}
 
